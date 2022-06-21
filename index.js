e841a0ce15c85406993cf98083612b005b285dae
Thelordgreat <greatokafor15@gmail.com>
2022-06-21 14:34:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = 'Great Okafor';
+const height = '6.2';
+const country = 'Nigeria';
+
+
+document.write('My name is ' + name + ' and I am ' + height + ' meters tall. I am from ' + country + '.');
+
+
